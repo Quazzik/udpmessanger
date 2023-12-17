b4896154816e4560f8b9bcadb091b7e8002731e5
Quazzik <Yuakorotkov@gmail.com>
2023-12-18 02:00:12 +0500

    added switchport

--- Logger/obj/Debug/net8.0/Logger.AssemblyInfo.cs
+++ Logger/obj/Debug/net8.0/Logger.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Logger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aede480d165eb81d7745e5e45a4a25362f04ba92")]
 [assembly: System.Reflection.AssemblyProductAttribute("Logger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Logger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Messanger/Program.cs
+++ Messanger/Program.cs
@@ -1,41 +1,64 @@
 ﻿using UDPService;
 
-Console.Write("ваш порт: ");
-ReadLine(out var localPort);
-Console.Write("порт для подключения: ");
-ReadLine(out var remotePort);
-Console.Write("Имя пользователя: ");
-var username = Console.ReadLine();
-Console.WriteLine("Для выхода напишите \"exit\"");
-if (string.IsNullOrWhiteSpace(username)) { username = "unnamed user"; Console.WriteLine(username); }
-
-var _udpService = new UdpService(localPort, remotePort);
-
-CancellationTokenSource cts = new CancellationTokenSource();
-var token = cts.Token;
-
-_ = Task.Run(async () =>
+namespace Messanger
 {
-    while (!token.IsCancellationRequested)
+    class Program
     {
-        var message = await _udpService.ReceiveMessageAsync();
+        private static int _remotePort = 22;
+        private static int _localPort = 22;
 
-        if (message == null)
-            continue;
-        Console.WriteLine(message);
+        static void Main(string[] args)
+        {
+            MainAsync();
+        }
+        static async void MainAsync()
+        {
+            Console.Write("Имя пользователя: ");
+            var username = Console.ReadLine();
+            Console.WriteLine("Для выхода напишите \"exit\"");
+            Console.WriteLine("Для смены порта напишите \"port\"");
+            if (string.IsNullOrWhiteSpace(username)) { username = "unnamed user"; Console.WriteLine(username); }
+
+            var _udpService = new UdpService(_localPort, _remotePort);
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            var token = cts.Token;
+
+            _ = Task.Run(async () =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    var message = await _udpService.ReceiveMessageAsync();
+
+                    if (message == null)
+                        continue;
+                    Console.WriteLine(message);
+                }
+            }, token);
+
+            await _udpService.SendMessageAsync(username, $"{username} connected");
+
+            while (!token.IsCancellationRequested)
+            {
+                var newMessage = Console.ReadLine();
+                if (newMessage == "port") { GetNewPorts(); _udpService = new UdpService(_localPort, _remotePort); continue; }
+                if (newMessage == "exit") { cts.Cancel(); continue; }
+                var createdMessage = await _udpService.SendMessageAsync(username, newMessage);
+            }
+        }
+        static void ReadLine(out int port)
+        {
+            while (!int.TryParse(Console.ReadLine(), out port)) { Console.Write("Неверный формат записи, введите номер порта: "); };
+        }
+        static void GetNewPorts()
+        {
+            Console.Write("ваш порт: ");
+            ReadLine(out _localPort);
+            Console.Write("порт для подключения: ");
+            ReadLine(out _remotePort);
+        }
     }
-}, token);
-
-await _udpService.SendMessageAsync(username, $"{username} connected");
-
-while (!token.IsCancellationRequested)
-{
-    var newMessage = Console.ReadLine();
-    if (newMessage == "exit") { cts.Cancel(); }
-    var createdMessage = await _udpService.SendMessageAsync(username, newMessage);
 }
 
-static void ReadLine(out int port)
-{
-    while (!int.TryParse(Console.ReadLine(), out port)) { Console.Write("Неверный формат записи, введите номер порта: "); };
-}
+
+
